b95dcd7774fe5a1702138a47019672b786c0518d
Matt Galindo <mgalindo15@pop-os.localdomain>
2025-05-12 23:17:12 -0500

    first js function

--- scripts/index.js
+++ scripts/index.js
@@ -1,3 +1,17 @@
-//Javascript Functions
+/* -------- Javascript Functions -------- */
 
-
+// Scrolldown Activates Details Section
+document.addEventListener("DOMContentLoaded", () => {
+    const overview = document.querySelector(".overview_title");
+  
+    const handleFirstScroll = () => {
+      if (overview) {
+        overview.classList.add("overview_animate");
+      }
+      // Clean Up
+      window.removeEventListener("scroll", handleFirstScroll);
+    };
+  
+    window.addEventListener("scroll", handleFirstScroll);
+  });
+  
