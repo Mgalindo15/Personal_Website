50686a678a666209ed5e2aec77cfe05bfdf7f3ee
Matt Galindo <mgalindo15@pop-os.localdomain>
2025-05-13 20:11:13 -0500

    added final structural features, some javascript

--- scripts/index.js
+++ scripts/index.js
@@ -1,17 +1,44 @@
 /* -------- Javascript Functions -------- */
 
-// Scrolldown Activates Details Section
+/* Scroll & Visibility Event Handlers for Title Animations */
 document.addEventListener("DOMContentLoaded", () => {
+    // For Profile Headings
     const overview = document.querySelector(".overview_title");
-  
-    const handleFirstScroll = () => {
-      if (overview) {
-        overview.classList.add("overview_animate");
+    const projects = document.querySelectorAll(".Project");
+    // Flag to track scrolling
+    let hasScrolled = false;
+    // funciton checks if Title is in viewport (visible)
+    function isInViewport(el) {
+      const rect = el.getBoundingClientRect();
+      return (
+        rect.top < window.innerHeight &&
+        rect.bottom > 0
+      );
+    }
+    //function that only triggers when scroll event happened at least once
+    function triggerVisibleProjects() {
+      if (!hasScrolled) return;
+        //will check piecewise visibility, trigger project title anim if true
+      projects.forEach((project) => {
+        if (isInViewport(project)) {
+          project.classList.add("visible");
+        }
+      });
+    }
+    // controller for scroll event
+    function handleScroll() {
+      if (!hasScrolled) {
+        hasScrolled = true;
+        //action to trigger overview title anim
+        if (overview) {
+          overview.classList.add("overview_animate");
+        }
       }
-      // Clean Up
-      window.removeEventListener("scroll", handleFirstScroll);
-    };
   
-    window.addEventListener("scroll", handleFirstScroll);
+      triggerVisibleProjects();
+    }
+  
+    //for refresh
+    window.addEventListener("scroll", handleScroll);
+    triggerVisibleProjects();
   });
-  
